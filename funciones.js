1faec493a8c0b65681c711b5a55e5017a50b2af1
basto115 <bastianasd3@gmail.com>
2024-05-16 20:49:27 -0400

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,34 @@
+
+    $(document).ready(function (){
+        let user = $("#user");
+        let contra = $("#contra");
+
+        let login = {
+          "username": "",
+          "password": ""
+
+        }
+        console.log(login)
+
+        $("#btnvali").click(function () {
+          $("#btnlim").toggle(500);
+          login.username = user.val();
+          login.password = user.val();
+          
+          console.log(login);
+
+        })
+        
+        contra.keydown(function(){
+          if (String(contra.val()).length <= 6){
+            contra.css("border", "3px solid red");
+          }else{
+            contra.css("border", "3px solid green");
+          }
+
+        })
+        
+
+    })
+
+
