45c6046e32374c82375bb7cd30f03f97dd61e594
basto115 <bastianasd3@gmail.com>
2024-05-17 02:32:40 -0400

    no se que hice, pero no funciona XD

--- funRegi.js
+++ funRegi.js
@@ -1,9 +1,69 @@
+const correo = document.getElementsById('correo');
+const passw = document.getElementsById('passw');
+const passw2 = document.getElementsById('passw2');
+const user2 = document.getElementsById('user2');
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = correo => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(correo).toLowerCase());
+}
+
+const validateInputs = () => {
+  const correoValue = correo.value.trim();
+  const passwValue = passw.value.trim();
+  const passw2Value = passw2.value.trim();
+
+  if(correoValue === '') {
+      setError(correo, 'Ingrese el correo electrónico');
+  } else if (!isValidEmail(correoValue)) {
+      setError(correo, 'Ingrese un correo electrónico válido');
+  } else {
+      setSuccess(correo);
+  }
+
+  if(passwValue === '') {
+      setError(passw, 'Ingrese la contraseña');
+  } else if (passwValue.length < 6 ) {
+      setError(passw, 'La contraseña debe contener al menos 8 caracteres.')
+  } else {
+      setSuccess(passw);
+  }
+
+  if(passw2Value === '') {
+      setError(passw2, 'Por favor confirmar contraseña');
+  } else if (passw2Value !== passwValue) {
+      setError(passw2, "Las contraseñas no coinciden");
+  } else {
+      setSuccess(passw2);
+  }
+
+};
+
 $(document).ready(function (){
     let nom = $("#nom");
     let ape = $("#ape");
     let user2 = $("#user2");
     let correo = $("#correo");
     let passw = $("#passw");
+    let passw2 = $("#passw2");
 
     let registro = {
       "nombre": "",
@@ -42,6 +102,16 @@ $(document).ready(function (){
         user2.css("border", "3px solid red");
       }else{
         user2.css("border", "3px solid green");
+      }
+
+    })
+
+    passw2.keydown(function(){
+      if (String(passw2.val()).length <= 6){
+        passw2.css("border", "3px solid red");
+        
+      }else{
+        passw2.css("border", "3px solid green");
       }
 
     })
